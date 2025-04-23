b88bcf091e8a5f27c2172e428b9caab5a51a843d
Lucas Garcia Arredondo <lucas.g.a@icloud.com>
2025-04-23 10:55:08 -0600

    Update algorithms.py

--- algorithms.py
+++ algorithms.py
@@ -3,20 +3,16 @@ from itertools import combinations
 
 
 def closure(attributes: set[Attribute], functional_dependencies: set[FunctionalDependency]) -> set[Attribute]:
-    closure_set = set(attributes)  # Empieza con los atributos dados
-    changed = True  # Controla si el cierre cambió en cada iteración
-
+    closure_set = set(attributes)
+    changed = True 
     while changed:
         changed = False
-        # Recorre todas las dependencias funcionales
         for fd in functional_dependencies:
-            # Si el determinante de la dependencia está en el cierre actual
-            if fd.determinant <= closure_set:  # Si fd.determinant es un subconjunto de closure_set
-                # Agrega los atributos dependientes al cierre
+            if fd.determinant <= closure_set:
                 new_elements = fd.dependant - closure_set
                 if new_elements:
                     closure_set.update(new_elements)
-                    changed = True  # Si el cierre cambia, establece 'changed' a True
+                    changed = True  
     return closure_set
 
 
@@ -27,7 +23,6 @@ def is_superkey(attributes: set[Attribute], heading: set[Attribute], functional_
 def is_key(attributes: set[Attribute], heading: set[Attribute], functional_dependencies: set[FunctionalDependency]) -> bool:
     if not is_superkey(attributes, heading, functional_dependencies):
         return False
-    # Verifica si hay un subconjunto propio que sea superclave  
     for i in range(1, len(attributes)):
         for subset in combinations(attributes, i):
             if is_superkey(set(subset), heading, functional_dependencies):
@@ -37,7 +32,6 @@ def is_key(attributes: set[Attribute], heading: set[Attribute], functional_depen
 
 def is_relvar_in_bcnf(relvar: Relvar):
     for fd in relvar.functional_dependencies:
-        # Si la dependencia funcional no es trivial y el determinante no es superclave
         if not fd.is_trivial() and not is_superkey(fd.determinant, relvar.heading, relvar.functional_dependencies):
             return False
     return True
@@ -47,7 +41,6 @@ def is_relvar_in_4nf(relvar: Relvar):
     if not is_relvar_in_bcnf(relvar):
         return False
     for mvd in relvar.multivalued_dependencies:
-        # Si la dependencia multivaluada no es trivial y el determinante no es superclave
         if not mvd.is_trivial(relvar.heading) and not is_superkey(mvd.determinant, relvar.heading, relvar.functional_dependencies):
             return False
     return True
